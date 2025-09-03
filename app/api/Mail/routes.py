318f8754438580fe17d33eb36ae2d5a6e9ccdc90
AlanMC7650 <alan.maldonado@ucb.edu.bo>
2025-09-02 22:09:45 -0400

    cambios

--- app/api/Mail/routes.py
+++ app/api/Mail/routes.py
@@ -2,7 +2,6 @@ from flask import Blueprint, request, jsonify, current_app
 import smtplib
 from email.message import EmailMessage
 import os
-
 from . import mail_bp as email_bp
 
 @email_bp.route("/", methods=["POST"])
@@ -11,19 +10,19 @@ def send_email():
     subject = data.get("subject")
     body = data.get("body")
 
-    # Configuración
+    
     sender = "alanarielmaldonadocarvajal1@gmail.com"
     app_password = "fhzy qcdf jvyw vqbk"
-    recipients = ["alanmaldonadoc5@gmail.com", "alan.maldonado@ucb.edu.bo"]
+    recipients = ["andresguti2003@gmail.com"]
 
-    # Crear mensaje
+    
     msg = EmailMessage()
     msg["From"] = sender
     msg["To"] = ", ".join(recipients)
     msg["Subject"] = subject
     msg.set_content(body)
 
-    # ✅ Construir ruta absoluta al archivo en static
+
     file_path = os.path.join(current_app.root_path, "static", "ramires.jpeg")
 
     with open(file_path, "rb") as f:
